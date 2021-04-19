5189cec2c25ef058ba5baee6253997d4619ed3f8
Catherine Smith <catsmith24601@gmail.com>
2021-04-19 16:29:46 -0400

    added inquirier array of prompts to index.js

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What is the name of your project?',
+      name: 'project',
+    },
+    {
+      type: 'input',
+      message: 'Please write a short description of your project.',
+      name: 'description',
+    },
+    {
+      type: 'list',
+      message: 'What kind of license should your project have?',
+      name: 'license',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      message: 'What command should be run to install dependencies?',
+      name: 'dependencies',
+      default: 'npm i',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'repo',
+    },
+    {
+        type: 'input',
+        message: 'EWhat does the user need to know about contributing to the repo?',
+        name: 'contributing',
+    },
+  ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
