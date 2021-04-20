a29256d17bb630f2db977534bb21411d32972094
Catherine Smith <catsmith24601@gmail.com>
2021-04-20 16:42:06 -0400

    added license badge info to generateMarkdown.js, added generateReadme function using template literal in index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// Globally scoped variables
 //Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
+// Creates a writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       message: 'What is your GitHub username?',
@@ -32,6 +38,8 @@ inquirer
       message: 'What kind of license should your project have?',
       name: 'license',
       choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    //   sets the default to the first object in the choices array in the line above (which is MIT license)
+      default: 0,
     },
     {
       type: 'input',
@@ -41,7 +49,7 @@ inquirer
     },
     {
         type: 'input',
-        message: 'What command should be run to run tests?',
+        message: 'What command should be used to run tests?',
         name: 'test',
         default: 'npm test',
     },
@@ -52,7 +60,7 @@ inquirer
     },
     {
         type: 'input',
-        message: 'EWhat does the user need to know about contributing to the repo?',
+        message: 'What does the user need to know about contributing to the repo?',
         name: 'contributing',
     },
   ]);
@@ -60,8 +68,51 @@ inquirer
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+// (Arrow) function to dynamically create the README file with template literal
+const generateReadme = (answers) =>
+`
+// license badge stuff here = with generateMarkdown.js
+
+# ${answers.project}
+
+## Description
+${answers.description}
+
+## Table of Contents
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [License](#License)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+
+## Installation
+To install necessary dependencies, run the following command${answers.dependencies}
+
+## Usage
+${answers.repo}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+To run tests, run the following command: ${answers.test}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at ${answers.username}.
+`
+
+
 // TODO: Create a function to initialize app
 function init() {}
+    promptUser()
+      .then((answers) => writeFileAsync('README.md', generateReadme(answers)));
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
 
-// Function call to initialize app
+// This function call initializes the app.
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,14 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
+
+// Code for the badges
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
+// [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0);
+// [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0);
+// [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause);
+
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -22,4 +30,5 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+// Exports this 
+module.exports = generateMarkdown;s
